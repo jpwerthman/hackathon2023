e260fcd8d939ace4656e0a9662c72e3ffc243799
MoyeshKhanal <MoyeshKhanal@gmail.com>
2023-10-14 16:25:11 -0500

    backend talks to frontend

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -32,17 +32,18 @@ const Home = () => {
 
       async function callAPI() {
         try {
-          const response = await fetch("https://talk-bot.onrender.com/", {
+          const response = await fetch("http://localhost:8000/message", {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({ message: inputPrompt }),
           });
           const data = await response.json();
+          console.log(data);
           setChatLog([
             ...chatLog,
             {
               chatPrompt: inputPrompt,
-              botMessage: data.botResponse,
+              botMessage: data.message,
             },
           ]);
           setErr(false);
