8bee9c73e620aa1a6acea1e0edb12fb6801b04bf
jonny werthman <j.p.werthman@wustl.edu>
2023-10-14 13:40:40 -0500

    destroyed backend

--- frontend-hackathon/src/App.js
+++ frontend-hackathon/src/App.js
@@ -1,22 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
+const [transactions, setTransactions] = useState([]);
+
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {transactions}
         </p>
-        <a
+        {/* <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
       </header>
     </div>
   );
