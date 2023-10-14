b3400a4c50cd0da9f5b336bb940025e770ff5a5c
MoyeshKhanal <MoyeshKhanal@gmail.com>
2023-10-14 16:29:50 -0500

    new commit

--- Backend/main.py
+++ Backend/main.py
@@ -1,6 +1,24 @@
 from fastapi import FastAPI
+from models import MessageResponse, UserMessage
+from fastapi.responses import JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
+
+
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:3000",
+    "http://localhost:3001",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Define a route using a decorator
 @app.get("/")
@@ -8,6 +26,9 @@ def read_root():
     return {"message": "Hello, FastAPI!"}
 
 # Define another route with a dynamic path parameter
-@app.get("/items/{item_id}")
-def read_item(item_id: int, query_param: str = None):
-    return {"item_id": item_id, "query_param": query_param}
+@app.post("/message")
+def read_item(message: UserMessage) -> dict:
+    print(message.message)
+    response_msg = MessageResponse(status=200, message=message.message)
+    response_data = {'data': response_msg.dict()}  # Convert MessageResponse to a dictionary
+    return response_data
